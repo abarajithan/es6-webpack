69360937d07c3c5037a2e9b6c81f6cdf23100b16
Abarajithan Ramachandran <abarajithan.r@olacabs.com>
2019-04-30 17:06:46 +0530

    Basic setup of ES6 with Webpack 1. Added spread operator 2.Added CSS config 3.Sample Fetch Call

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import './css/main.css';
+
+window.changeDetails = () => {
+    let value = {
+        details : {}
+    }
+    document.getElementById('change-name').innerText = "Hello John";
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => {
+        value.details = {
+            ...json
+        }
+        console.log("Value:",value)
+    })
+
+}
+
+    
