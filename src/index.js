f6ef1deeec1755a129400e2542632a462e935f4e
Abarajithan Ramachandran <abarajithan.r@olacabs.com>
2021-03-05 09:48:54 +0530

    Simple javascrip logger with Middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,41 @@
 import './styles/main.scss';
 
+
+window.initializeLogger = function (){
+    return{
+        stack:{},
+        add:function(fn){
+            this.stack[Object.keys(this.stack).length] = fn;
+            return Object.keys(this.stack).length - 1;
+        },
+        remove: function(index){
+            delete this.stack[index]
+        },
+        log: function(msg){
+            for(let index in this.stack){
+                msg = this.stack[index].apply(this,[msg]);
+            }
+            console.log(msg);
+        }
+    }
+}
+
+window.logger = window.initializeLogger();
+
+logger.log("Hello");
+let md1 = logger.add(function(msg){
+    return msg+msg;
+});
+logger.log("Hello");
+let md2 = logger.add(function(msg){
+    return msg+"123";
+});
+logger.log("Hello");
+logger.remove(md1);
+logger.log("Hello");
+logger.remove(md2);
+logger.log("Hello");
+
 window.changeDetails = () => {
     let value = {
         details : {}
@@ -13,7 +49,6 @@ window.changeDetails = () => {
         }
         console.log("Value:",value)
     })
-
 }
 
     
